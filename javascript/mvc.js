b3dde0893c8ef58b3e3e2a739841f6af86ea5adf
pulkit <pulkit.pushkarna@gmail.com>
2015-09-08 20:21:05 +0530

    First commit

---
+++ javascript/mvc.js
@@ -0,0 +1,6 @@
+var myApp=angular.module("MyApp",[])
+
+myApp.controller("MyController",function($scope){
+$scope.msg="Hello world"
+
+})
---
+++ javascript/mvvm.js
@@ -0,0 +1,5 @@
+var myApp=angular.module("MyApp",[])
+
+myApp.controller("MyController",function($scope){
+$scope.msg="Hello world";
+})
