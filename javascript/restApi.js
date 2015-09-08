d9952b338bf71d2902497763c3eb39d3341253ab
Manish <manish@intelligrape.com>
2015-09-09 03:10:58 +0530

    Added demo for REST Api Interaction

---
+++ javascript/restApi.js
@@ -0,0 +1,27 @@
+var app = angular.module('app', []);
+
+app.service('taskService', function($http) {
+	this.fetchTaskList = function(onComplete, onError){
+		$http({
+			method: 'POST',
+			url: "http://localhost:8080/worker/task/list",
+			params: {}
+		}).then(onComplete, onError);	
+	}
+	
+});
+
+app.controller('TaskListController', function($scope, taskService){
+	
+	$scope.taskList=[];
+
+	$scope.getTaskList = function(){
+		var onComplete=function(response){
+			$scope.taskList = response.data;
+		}
+		var onError = function(reason){
+			console.log("reason: ", reason)
+		}
+		taskService.fetchTaskList(onComplete, onError)
+	}
+})
